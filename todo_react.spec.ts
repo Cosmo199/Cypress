455dc8ea3045e32bad2439d43e8e570bc773790d
nawakarn ninparwit <karn939.n@gmail.com>
2020-06-26 13:31:34 +0700

    Update todo_react.spec.ts

--- todo_react.spec.ts
+++ todo_react.spec.ts
@@ -7,20 +7,18 @@ describe('todo vue test',() => {
         cy.visit(reactTodo);
       });
 
-      it('can add new todo',() =>{
-        const todo = 'Test React Todo with Cypress'
+      it('can add new todo',() => {
+        const todo = 'Test React Todo with Cypress';
         cy.get(locators.reactTodo.newTodoInput).type(`${todo}{enter}`);
         cy.get(locators.reactTodo.todoLabel).first().should('have.text',todo);
       });
 
-      it('can add new todo',() =>{
+      it('should be able to delete todo',() => {
         const todo = 'will be deleted'
         cy.get(locators.reactTodo.newTodoInput).type(`${todo}{enter}`);
         cy.get(locators.reactTodo.toggleAll).click();
         cy.get(locators.reactTodo.clearCompleted).click();
-        cy.get(locators.reactTodo.todoList).should('not.de.visible');
+        cy.get(locators.reactTodo.todoList).should('not.be.visible');
 
     });
-
-
 });
